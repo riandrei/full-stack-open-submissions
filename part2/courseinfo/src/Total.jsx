d146d9015e485031fca9b9893b64a611ce02cfef
Riandrei Casañas <riandrei.casanas@gmail.com>
2023-06-08 18:35:48 +0800

    Completed exercise 2.3
    No code changes because I've already done the requirement on my last commit

--- part2/courseinfo/src/Total.jsx
+++ part2/courseinfo/src/Total.jsx
@@ -1,4 +1,5 @@
 const Total = ({ parts }) => {
+  // adding this so I can make a commit without changes
   return <h4>total of {parts.reduce((total, part) => total + part.exercises, 0)} exercises</h4>;
 };
 
