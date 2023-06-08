2c4658f5d4511875e4f3dc79fe189d2536b3a655
Riandrei Casañas <riandrei.casanas@gmail.com>
2023-06-08 18:41:09 +0800

    Completed exercise 2.4
    Modified the App component to work regardless of number of courses

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -4,11 +4,11 @@ import Total from './Total';
 
 const Course = ({ course }) => {
   return (
-    <div>
+    <>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
-    </div>
+    </>
   );
 };
 export default Course;
