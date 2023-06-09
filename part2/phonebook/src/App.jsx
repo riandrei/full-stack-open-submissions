9dbe680777d56ad1ea4f0d74dafa7b6da7f8d86d
Riandrei Casañas <riandrei.casanas@gmail.com>
2023-06-09 19:02:42 +0800

    Completed exercise 2.11
    Created a 'database' where the initial state is coming from

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Persons from './Persons';
 import PersonForm from './PersonForm';
 import SearchForm from './SearchForm';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchEntry, setSearchEntry] = useState('');
+
+  useEffect(() => {
+    const fetchPeople = axios.get('http://localhost:3001/persons');
+
+    fetchPeople.then((response) => setPersons(response.data));
+  }, []);
 
   return (
     <>
