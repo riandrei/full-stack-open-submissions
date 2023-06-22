b3e28a7f1ce710884f3fee08d121a06a164730ec
Riandrei Casañas <riandrei.casanas@gmail.com>
2023-06-22 16:08:39 +0800

    Completed exercise 2.20
    Added weather info to the CountryDetails

--- part2/countries/src/components/CountryDetails.jsx
+++ part2/countries/src/components/CountryDetails.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+
+import countriesService from "../services/countriesService";
+
 const CountryDetails = ({ filteredCountry }) => {
+  const [countryWeather, setCountryWeather] = useState(null);
+
+  useEffect(() => {
+    countriesService
+      .getCountryWeather(filteredCountry.capital)
+      .then((data) => setCountryWeather(data));
+  }, []);
+
   return (
     <div>
       <h1>{filteredCountry.name.common}</h1>
@@ -11,6 +23,19 @@ const CountryDetails = ({ filteredCountry }) => {
         ))}
       </ul>
       <img src={filteredCountry.flags.png} alt={filteredCountry.flags.alt} />
+      <h2>Weather in {filteredCountry.capital}</h2>
+      {!countryWeather ? (
+        <p>Weather loading...</p>
+      ) : (
+        <>
+          <p>temperature {countryWeather.main.temp} Celcius</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${countryWeather.weather[0].icon}@2x.png`}
+            alt=""
+          />
+          <p>wind {countryWeather.wind.speed} m/s</p>
+        </>
+      )}
     </div>
   );
 };
--- part2/countries/src/services/countriesService.js
+++ part2/countries/src/services/countriesService.js
@@ -8,4 +8,13 @@ const getCountries = () => {
   return request.then((response) => response.data);
 };
 
-export default { getCountries };
+const getCountryWeather = (capital) => {
+  const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+  const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${apiKey}`;
+
+  const request = axios.get(openWeatherUrl);
+
+  return request.then((response) => response.data);
+};
+
+export default { getCountries, getCountryWeather };
